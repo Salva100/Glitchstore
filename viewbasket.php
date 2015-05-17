098928f6d3c0ed5aa274f3a9e90dec428579ce6a
Valentin Granados <valefcfhc@gmail.com>
2015-05-17 10:42:25 -0500

    viewbasket.php

--- viewbasket.php
+++ viewbasket.php
@@ -1,3 +1,50 @@
+<?php 
+    /*include_once("../FuncionesPHP/bdconnect.php");
+    include_once("../FuncionesPHP/bdaccess.php");    
+    include_once("../FuncionesPHP/productos.php");*/
+
+    require_once("conekta-php/lib/Conekta.php");
+    Conekta::setApiKey("key_yu7cz6iTH3dv3cqtxedaUA");
+    Conekta::setLocale('es');
+
+    $precio = 50000;
+    $precioGanancia = $precio * .05;
+    $precioGananciaReal = $precioGanancia/2;
+    $precioLocatario = $precio - $precioGanancia;
+    $glitchPoints = $precioGananciaReal;// * 10;
+
+    /*
+    echo "$precio<br>"; 
+    echo "$precioGanancia<br>";
+    echo "$precioGananciaReal<br>";
+    echo "$precioLocatario<br>";
+    echo "$glitchPoints<br>";  
+    */
+
+    try{
+
+        $chargePoints = Conekta_Charge::create(array(
+          "amount"=> $glitchPoints,
+          "currency"=> "MXN",
+          "description"=> "Puntos ganados",
+          "reference_id"=> "GlitchPoints",
+          "card"=> $_POST['conektaTokenId'],
+       //"tok_a4Ff0dD2xYZZq82d9",
+          "details"=> array(
+            "email"=>"valefcfhc@gmail.com"
+            )
+        ));
+
+        echo $chargePoints->status;
+
+      }catch (Conekta_Error $e){
+        echo $e->getMessage();
+       //el pago no pudo ser procesado
+      }
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,6 +56,77 @@
 <!--[if IE]>
   <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
+
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript" src="https://conektaapi.s3.amazonaws.com/v0.3.2/js/conekta.js"></script>
+<script type="text/javascript">
+ 
+ // Conekta Public Key
+  Conekta.setPublishableKey('key_yu7cz6iTH3dv3cqtxedaUA');
+ 
+ // ...
+
+</script>
+<script type="text/javascript">
+  var conektaSuccessResponseHandler;
+  conektaSuccessResponseHandler = function(token) {
+    console.log("token")
+    var $form;
+    $form = $("#card-form");
+
+    console.log("token0")
+  /* Inserta el token_id en la forma para que se envíe al servidor */
+
+    $form.append($("<input type=\"hidden\" name=\"conektaTokenId\" />").val(token.id));
+
+    console.log("token1")
+  /* and submit */
+
+    $form.get(0).submit();
+
+    console.log("token2")
+  };
+
+  var conektaErrorResponseHandler;
+  conektaErrorResponseHandler = function(response) {
+    var $form;
+    $form = $("#card-form");
+
+    console.log("error")
+    
+  /* Muestra los errores en la forma */
+
+    $form.find(".card-errors").text(response.message);
+    $form.find("button").prop("disabled", false);
+
+  };
+
+
+  jQuery(function($) {
+    $("#card-form").submit(function(event) {
+      var $form;
+      $form = $(this);
+
+      console.log("hoa")
+      
+  /* Previene hacer submit más de una vez */
+
+      $form.find("button").prop("disabled", true);
+
+      Conekta.token.create($form, conektaSuccessResponseHandler, conektaErrorResponseHandler);
+
+  /* Previene que la información de la forma sea enviada al servidor */
+
+      return false;
+
+      console.log("hoa3")
+    });
+  });
+
+</script>
+
+
 </head>
 <body>
 <header>
@@ -68,7 +186,7 @@
     </div>
 </aside>
 <article id="basket">
-<button class="continue">Pay securely now</button>
+
 <h1>Shopping Bag</h1>
 <table width="100" border="1">
     <tr>
@@ -76,42 +194,7 @@
         <th scope="col" class="options">Options</th>
         <th align="right" scope="col" class="price">Price</th>
     </tr>
-    <tr>
-        <td align="left" valign="top" class="description"><a href="main.html"><img src="images/thumb1.jpg" alt="Elegant evening Dress" class="left" /></a><p><a href="main.html">Elegant evening Dress</a><br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla volutpat ultricies fringilla. Suspendisse iaculis tristique leo, id adipiscing massa aliquet ut. Etiam adipiscing auctor enim nec tincidunt. </p>
-        <a href="#">Remove</a></td>
-        <td align="left" valign="top" class="options">
-        	<dl>
-                <dt>Product ID</dt>
-                <dd>6442567</dd>
-                <dt>Colour:</dt>
-                <dd>Light blue</dd>
-                <dt>Size:</dt>
-                <dd>S</dd>
-                <dt>Quantity:</dt>
-                <dd>1</dd>
-            </dl>
-            <button>Change details</button>
-        </td>
-        <td align="right" valign="top" class="price">&pound;249</td>
-    </tr>
-    <tr>
-    	<td align="left" valign="top" class="description"><a href="main.html"><img src="images/thumb1.jpg" alt="Elegant evening Dress" class="left" /></a><p><a href="main.html">Elegant evening Dress</a><br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla volutpat ultricies fringilla. Suspendisse iaculis tristique leo, id adipiscing massa aliquet ut. Etiam adipiscing auctor enim nec tincidunt. </p>
-        <a href="#">Remove</a></td>
-        <td align="left" valign="top" class="options">
-        	<dl>
-                <dt>Product ID</dt>
-                <dd>1936246</dd>
-                <dt>Colour:</dt>
-                <dd>Light blue</dd>
-                <dt>Size:</dt>
-                <dd>S</dd>
-                <dt>Quantity:</dt>
-                <dd>1</dd>
-            </dl>
-            <button>Change details</button>
-        </td>
-        <td align="right" valign="top" class="price">&pound;249</td>
-    </tr>
+    
     <tr>
     	<td align="left" valign="top" class="description"><a href="main.html"><img src="images/thumb1.jpg" alt="Elegant evening Dress" class="left" /></a><p><a href="main.html">Elegant evening Dress</a><br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla volutpat ultricies fringilla. Suspendisse iaculis tristique leo, id adipiscing massa aliquet ut. Etiam adipiscing auctor enim nec tincidunt. </p>
         <a href="#">Remove</a></td>
@@ -132,6 +215,50 @@
     </tr>
 </table>
 <img src="images/creditcards.gif" class="safe" />
+
+<!-- FORMULARIO PARA REALIZAR LAS COMPRAS -->
+
+<div class="left" id="ProductInfo">        
+                    
+                    Generales<br>
+                    Precio<br>
+                      <form action="" method="POST" id="card-form">
+                      <span class="card-errors"></span>
+                      <div class="form-row">
+                        <label>
+                          <span>Nombre del tarjeta habiente</span>
+                          <input type="text" size="20" data-conekta="card[name]"/>
+                        </label>
+                      </div>
+                      <div class="form-row">
+                        <label>
+                          <span>Numero de tarjeta de credito</span>
+                          <input type="text" size="20" data-conekta="card[number]"/>
+                        </label>
+                      </div>
+                      <div class="form-row">
+                        <label>
+                          <span>CVC</span>
+                          <input type="text" size="4" data-conekta="card[cvc]"/>
+                        </label>
+                      </div>
+                      <div class="form-row">
+                        <label>
+                          <span>Fecha de expiracion (MM/AAAA)</span>
+                          <input type="text" size="2" data-conekta="card[exp_month]"/>
+                        </label>
+                        <span>/</span>
+                        <input type="text" size="4" data-conekta="card[exp_year]"/>
+                      </div>
+                      
+                      <button type="submit" class="continue">Pay securely now</button>
+
+                    </form>
+
+                </div>
+
+
+
 <div class="right">
     <strong>Subtotal before Delivery Charges</strong> <em>&pound;398.00</em><br />
     <p>
@@ -142,7 +269,8 @@
     </p>
     <strong>Your total</strong> <em>&pound;398.00</em>
 </div>
-<button class="continue">Pay securely now</button>
+
+
 </article>
 <footer>
 	<div class="wrapper">
