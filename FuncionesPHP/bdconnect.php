74ec215efd72c324c9a58197e2ed72f3a372b398
Richard K Rivero <rk_rivero@outlook.com>
2015-09-20 20:25:54 -0500

    GS
    First Look

--- FuncionesPHP/bdconnect.php
+++
@@ -1,9 +0,0 @@
-<?php	
-	$GLOBALS["servidor"] = "localhost"; 
-	$GLOBALS["usuario"] = "root";  
-	$GLOBALS["contrasena"] = "majora";  
-	$GLOBALS["bd"] = "gsbd";
-
-	$con = mysql_connect($GLOBALS["servidor"],$GLOBALS["usuario"],$GLOBALS["contrasena"]); 
-	mysql_select_db($GLOBALS["bd"],$con); 
-?>
